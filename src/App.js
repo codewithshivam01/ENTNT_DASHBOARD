a535aa3cdca4277258e94475ac62f5833888c5fe
SHIVAM SINGH <shivamchoudhary16102001@gmail.com>
2025-05-30 00:10:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
-import React, { useContext } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthContext } from './contexts/AuthContext';
-import LoginPage from './pages/LoginPage';
-import DashboardPage from './pages/DashboardPage';
-import EquipmentPage from './pages/EquipmentPage';
-import EquipmentDetailPage from './pages/EquipmentDetailPage';
-import RentalsPage from './pages/RentalsPage';
-import MaintenancePage from './pages/MaintenancePage';
-import ProtectedRoute from './components/ProtectedRoute';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  const { user } = useContext(AuthContext);
-
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<Navigate to="/dashboard" />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/equipment" element={<EquipmentPage />} />
-          <Route path="/equipment/:id" element={<EquipmentDetailPage />} />
-          <Route path="/rentals" element={<RentalsPage />} />
-          <Route path="/maintenance" element={<MaintenancePage />} />
-        </Route>
-        <Route path="*" element={<Navigate to={user ? "/" : "/login"} />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="text-center items-center" >
+    
+      hello
+    </div>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import { AuthProvider } from './contexts/AuthContext';
-import { EquipmentProvider } from './contexts/EquipmentContext';
-import { RentalsProvider } from './contexts/RentalsContext';
-import { MaintenanceProvider } from './contexts/MaintenanceContext';
-import './styles/main.css';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <AuthProvider>
-    <EquipmentProvider>
-      <RentalsProvider>
-        <MaintenanceProvider>
-          <App />
-        </MaintenanceProvider>
-      </RentalsProvider>
-    </EquipmentProvider>
-  </AuthProvider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
